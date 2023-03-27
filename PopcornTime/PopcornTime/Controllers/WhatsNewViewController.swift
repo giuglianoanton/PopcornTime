7352843d507883d16c52cfb1d3f37e8172e5638c
Antonella Giugliano <giuglianoanton@gmail.com>
2023-03-27 16:54:34 +0200

    add table view with cells

--- PopcornTime/PopcornTime/Controllers/WhatsNewViewController.swift
+++ PopcornTime/PopcornTime/Controllers/WhatsNewViewController.swift
@@ -8,14 +8,30 @@
 import UIKit
 
 class WhatsNewViewController: UIViewController {
-
+    
+    
+    private let table: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        
         view.backgroundColor = .systemBackground
+        view.addSubview(table)
+        
+        table.delegate =  self
+        table.dataSource = self
     }
+    
+    
+    // set the frame of the table
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        table.frame = view.bounds
+    }
+
     
 
     /*
@@ -29,3 +45,19 @@ class WhatsNewViewController: UIViewController {
     */
 
 }
+
+extension WhatsNewViewController: UITableViewDelegate, UITableViewDataSource{
+
+     // set the numer of rows you wnat to see
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return 20
+     }
+
+     // set the number of cell per row you want to see
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+         cell.textLabel?.text = "Test"
+         return cell
+     }
+
+ }
