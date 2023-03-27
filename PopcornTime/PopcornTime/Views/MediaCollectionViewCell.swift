01ac0433063285809d76a8a755c706d8ca50f198
Antonella Giugliano <giuglianoanton@gmail.com>
2023-03-27 22:49:07 +0200

    add package dependency and creates Media Collection cell

---
+++ PopcornTime/PopcornTime/Views/MediaCollectionViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  MediaCollectionViewCell.swift
+//  PopcornTime
+//
+//  Created by Antonella Giugliano on 27/03/23.
+//
+
+import UIKit
+
+// a thir part library that allows to store in the cache the images you fetch from the api
+import SDWebImage
+
+class MediaCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "MediaCollectionViewCell"
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+    
+    // set the poster in the cache
+    public func configure(with poster: String) {
+        guard let url = URL(string: poster) else {return}
+        posterImageView.sd_setImage(with: url, completed: nil)
+    }
+}
