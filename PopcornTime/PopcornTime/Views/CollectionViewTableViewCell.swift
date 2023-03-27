8c98f46c0b9154cffe10a6edcdd6454b7c8918ac
Antonella Giugliano <giuglianoanton@gmail.com>
2023-03-27 17:12:10 +0200

    configure the cells in each section

--- PopcornTime/PopcornTime/Views/CollectionViewTableViewCell.swift
+++ PopcornTime/PopcornTime/Views/CollectionViewTableViewCell.swift
@@ -8,28 +8,58 @@
 import UIKit
 
 class CollectionViewTableViewCell: UITableViewCell {
-
-     static let identifier = "CollectionViewTableViewCell"
-
-     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-         super.init(style: style, reuseIdentifier: reuseIdentifier)
-         contentView.backgroundColor = .systemIndigo
-     }
-
-     required init?(coder: NSCoder) {
-         fatalError()
-     }
-
-     /*
+    
+    static let identifier = "CollectionViewTableViewCell"
+    
+    private let collectionView: UICollectionView = {
+        
+        // set the layout of the collection view
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 166, height: 287)
+        layout.scrollDirection = .horizontal
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        return cv
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemIndigo
+        contentView.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    //set frame for the cell
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
+    /*
      override func awakeFromNib() {
-         super.awakeFromNib()
-         // Initialization code
-     }
-
-     override func setSelected(_ selected: Bool, animated: Bool) {
-         super.setSelected(selected, animated: animated)
-
-         // Configure the view for the selected state
+     super.awakeFromNib()
+     @@ -33,3 +55,17 @@ class CollectionViewTableViewCell: UITableViewCell {
      }*/
+    
+}
 
- }
+extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    // set the cell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemMint
+        return cell
+    }
+    // number of the cells
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+}
