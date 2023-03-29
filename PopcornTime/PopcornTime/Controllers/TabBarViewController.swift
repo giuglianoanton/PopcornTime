ea764b00f5668a6520fb91743f85352f130fba5b
Antonella Giugliano <giuglianoanton@gmail.com>
2023-03-29 16:51:45 +0200

    congrue the nabber for myList view

--- PopcornTime/PopcornTime/Controllers/TabBarViewController.swift
+++ PopcornTime/PopcornTime/Controllers/TabBarViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import SwiftUI
 
 class TabBarViewController: UITabBarController {
     
     // create a navigation controller for each of the vcs
       let whatsNew =  UINavigationController(rootViewController: WhatsNewViewController())
       let myList =  UINavigationController(rootViewController: MyListViewController())
+//      let myList =  UIHostingController(rootView: MyListView())
 
 
     override func viewDidLoad() {
@@ -19,7 +21,7 @@ class TabBarViewController: UITabBarController {
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemTeal
         
-        configureNavBarWhatsNew()
+        configureNavBar()
         // set the tabBar
         self.tabBar.backgroundColor = .systemBackground
         whatsNew.tabBarItem = UITabBarItem(title: "What's New", image: UIImage(systemName: "popcorn.fill"), tag: 0)
@@ -29,8 +31,9 @@ class TabBarViewController: UITabBarController {
         setViewControllers([whatsNew, myList], animated: true)
     }
     
-    private func configureNavBarWhatsNew() {
+    private func configureNavBar() {
              whatsNew.navigationBar.prefersLargeTitles = true
+             myList.navigationBar.prefersLargeTitles = true
          }
 
     
