44723363b5e9f371f5898120388326ccfaad714a
Antonella Giugliano <giuglianoanton@gmail.com>
2023-03-29 16:49:29 +0200

    integrate the swiftui view in uikit

--- PopcornTime/PopcornTime/Controllers/MyListViewController.swift
+++ PopcornTime/PopcornTime/Controllers/MyListViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import SwiftUI
 
 class MyListViewController: UIViewController {
-
+    
+    var searchController = UISearchController()
+    var container = UIView()
+    
+    let myListView: UIHostingController = {
+        let myListView = UIHostingController(rootView: MyListView())
+        myListView.view.backgroundColor = .systemBackground
+        return myListView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        configureNavBar()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .systemBackground
+//        view.backgroundColor = .systemBackground
+        
+        container.frame = CGRect(x: container.bounds.origin.x, y: container.bounds.origin.y, width: view.frame.width, height: view.frame.height)
+        view.addSubview(container)
+        
+        addChild(myListView)
+        myListView.view.frame = container.bounds
+        container.addSubview(myListView.view)
+        myListView.didMove(toParent: self)
+        
+        
+    }
+    
+    private func configureNavBar(){
+        // navigation configuration
+        self.title = "My List"
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        
     }
     
 
