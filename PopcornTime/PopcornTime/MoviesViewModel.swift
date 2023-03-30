c0add5bd8f56f20520d7a031125eeb9c9b2c800c
Antonella Giugliano <giuglianoanton@gmail.com>
2023-03-31 00:56:33 +0200

    set the icon

---
+++ PopcornTime/PopcornTime/MoviesViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesViewModel.swift
+//  PopcornTime
+//
+//  Created by Antonella Giugliano on 29/03/23.
+//
+
+//import Foundation
+//
+//class MoviesViewModel: ObservableObject{
+//    @Published var movies: [Media]?
+//    
+//}
